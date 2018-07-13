bf0dfeb2812ba85fdb2b86b28e9d079995dd345f
sohammondal <soham.pf.mondal@gmail.com>
2018-07-13 11:42:45 +0530

    uopdated AssetData Component

--- frontend/src/components/AssestData.jsx
+++ frontend/src/components/AssestData.jsx
@@ -6,21 +6,89 @@ class AssetData extends Component{
         super(props);
         this.state = {
             asset_data:null,
-
+            isLoaded:false
         }
     }
 
-    getAssetData(){
-       
+    getAssetData(){        
+        fetch('https://6no17qcva0.execute-api.us-west-2.amazonaws.com/DevStage?EQUIP_NO='+this.props.equip_no  )
+        .then(resp => resp.json())
+         .then(
+             data => {                
+                this.setState({ asset_data:data, isLoaded:true })
+                
+             }
+         )
+    }
+   
+                    
+
+    componentDidMount(){
+        this.getAssetData();
     }
 
-
-    render(){
-        console.log('this.props',this.props);
-        return(
-            <div>
-                AssetDataCompUpdate
+    renderData(){
+        const { asset_data,isLoaded } = this.state;
+         
+            
+       // console.log("asset_data",this.state.asset_data);        
+        if(isLoaded)
+        {
+         return ( 
+            <div> 
+             
+            
+                 { asset_data.message.map( asset => {
+                        return( 
+                           <div className="card" key={asset.EQUIP_NO}> 
+                            <div className="card-header">Equipment NO: {asset.EQUIP_NO}</div>
+                            <div className="card-body">
+                              <p className=""><code>DSTRCT_CODE: {asset.DSTRCT_CODE}</code></p>
+                              <p className=""><code>EQUIP_CLASS: {asset.EQUIP_CLASS}</code></p>
+                              <p className=""><code>EQUIP_GRP_ID: {asset.EQUIP_GRP_ID}</code></p>
+                              <p className=""><code>EQUIP_LOCATION: {asset.EQUIP_LOCATION}</code></p>
+                              <p className=""><code>EQUIP_STATUS: {asset.EQUIP_STATUS}</code></p>
+                              <p className=""><code>ITEM_NAME_1: {asset.ITEM_NAME_1}</code></p>
+                              <p className=""><code>ITEM_NAME_2: {asset.ITEM_NAME_2}</code></p>
+                              <p className=""><code>ITEM_NAME_CODE: {asset.ITEM_NAME_CODE}</code></p>
+                              <p className=""><code>LOCATION: {asset.LOCATION}</code></p>
+                              <p className=""><code>PARENT_EQUIP: {asset.PARENT_EQUIP}</code></p>
+                              <p className=""><code>PLANT_NO: {asset.PLANT_NO}</code></p>
+                            </div>
+                            <div className='card-footer'>Equipment NO: {asset.EQUIP_NO}</div>
+                           </div>     
+                        )
+                    }) 
+                   
+                }
+                
+            </div> )
+        }
+        else{
+            <div> 
+               <p>No Data Available </p>
             </div>
+        }
+    }
+ 
+    render(){         
+        //console.log( 'TestData-',this.state.asset_data);
+       // console.log(this.state.asset_data.message.DSTRCT_CODE);
+        
+        return(
+<<<<<<< HEAD
+            <div>
+                AssetDataComp
+=======
+            
+            <div> 
+
+               {
+                   this.renderData()
+               }
+>>>>>>> origin/frontend2
+            </div>
+            
         )
     }
    
