75ec8ed71b2392ffc26be8d8b05b7d76c2b6363a
sohammondal <soham.pf.mondal@gmail.com>
2018-07-13 15:55:21 +0530

    shell script update

--- update-cdn.sh
+++ update-cdn.sh
@@ -1,6 +1,8 @@
 ssh ec2-user@35.177.21.226
 usDist=E6C5IVF7KYD9P
 ukDist=E2RH2JBC3UI9WB
+echo 'US Dist' $usDist
+ech  'UK Dist' $ukDist
 id1=$(aws cloudfront create-invalidation --paths /var/www/html/*  --distribution-id $usDist | grep Id | awk '{print $1 $2}' | cut -d : -f2 | sed 's/\"//g')
 id2=$(aws cloudfront create-invalidation --paths /var/www/html/*  --distribution-id $ukDist | grep Id | awk '{print $1 $2}' | cut -d : -f2 | sed 's/\"//g')
 echo 'Invalidation ID for US CDN is ' $id1
